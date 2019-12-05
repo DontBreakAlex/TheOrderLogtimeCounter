ab17ed098b5bc49795286f51983e602123fd4573
Alexandre Seo <perso@alexandre-seo.tk>
2019-12-05 15:34:14 +0100

    Better update

--- TheOrderLogtimeCounter.user.js
+++ TheOrderLogtimeCounter.user.js
@@ -1,37 +1,40 @@
 // ==UserScript==
 // @name     The Order logtime counter
-// @version  1.0.0
+// @version  1.0.1
 // @include  https://profile.intra.42.fr/
 // @run-at   document-idle
 // @license  GPL-3.0-or-later
 // @author   aseo
+// @updateURL https://openuserjs.org/meta/DontBreakAlex/The_Order_logtime_counter.meta.js
+// @downloadURL https://openuserjs.org/install/DontBreakAlex/The_Order_logtime_counter.user.js
 // ==/UserScript==
 
-setTimeout(firstRun, 500);
-
-function firstRun() {
-	updateLogtime();
-	setInterval(updateLogtime, 60000);
-}
+updateLogtime();
+setInterval(updateLogtime, 60000);
 
 function updateLogtime() {
-
 	let logs = document.getElementById("user-locations");
-	let index = logs.children.length, counter = 0, elem, logtime = [0, 0];
-	if (index != 0) {
+	let index = logs.children.length, counter = 0, elem;
+	if (index == 0) {
+		setTimeout(updateLogtime, 500);
+		return ;
+	}
+	if (updateLogtime.logtime == undefined) {
+		updateLogtime.logtime = [0, 0];
 		index--;
 		while (counter != 7 && index > 0) {
 			elem = logs.children[index];
 			if (elem.tagName == 'g') {
 				counter++;
-				elem.dataset.originalTitle.split('h').forEach((elm, i) => {logtime[i] += Number(elm)})
+				elem.dataset.originalTitle.split('h').forEach((elm, i) => {updateLogtime.logtime[i] += Number(elm)})
 			}
 			index--;
 		}
-		while (logtime[1] >= 60) {
-			logtime[1]-= 60;
-			logtime[0]++;
-		}
-		logs.parentElement.children[0].innerHTML = "LOGTIME: " + logtime[0] + "h" + ("0" + logtime[1]).slice(-2);
 	}
+	else updateLogtime.logtime[1]++;
+	while (updateLogtime.logtime[1] >= 60) {
+		updateLogtime.logtime[1] -= 60;
+		updateLogtime.logtime[0]++;
+	}
+	logs.parentElement.children[0].innerHTML = "LOGTIME: " + updateLogtime.logtime[0] + "h" + ("0" + updateLogtime.logtime[1]).slice(-2);
 }
