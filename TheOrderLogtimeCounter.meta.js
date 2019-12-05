af220d03cceca7b61899634e4af258e2e14ab960
Alexandre Seo <perso@alexandre-seo.tk>
2019-12-05 15:50:37 +0100

    Je crois qu'il y a besoin de ca

---
+++ TheOrderLogtimeCounter.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name     The Order logtime counter
+// @version  1.0.1
+// @include  https://profile.intra.42.fr/
+// @run-at   document-idle
+// @license  GPL-3.0-or-later
+// @author   aseo
+// @updateURL https://openuserjs.org/meta/DontBreakAlex/The_Order_logtime_counter.meta.js
+// @downloadURL https://openuserjs.org/install/DontBreakAlex/The_Order_logtime_counter.user.js
+// ==/UserScript==
